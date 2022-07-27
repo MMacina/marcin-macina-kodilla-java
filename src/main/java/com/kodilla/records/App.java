8fee46874a63623c31bb2d4e50672a2d0225fd97
Marcin <marcin.macina@outlook.com>
2022-07-27 13:04:14 +0200

    DataClass

---
+++ src/main/java/com/kodilla/records/App.java
@@ -0,0 +1,7 @@
+package com.kodilla.records;
+
+public class App {
+    public static void main(String[] args) {
+        Book book = new Book("John Nowak", "Algorithms and other nightmares", 2018);System.out.println(book);
+    }
+}
