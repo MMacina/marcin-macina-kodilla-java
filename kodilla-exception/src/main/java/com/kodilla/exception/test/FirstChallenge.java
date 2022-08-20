af6d5eddac3359073b26dfbdc7632c1e5dc6b81f
Marcin <marcin.macina@outlook.com>
2022-08-20 18:08:25 +0200

    8.2 - try-catch-finally_update

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,22 +3,23 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
     public double divide(double a, double b) {
 
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-        } catch (ArithmeticException ao) {
-            System.out.println("The divider can't equals 0. Chose different divider " + "(\"" + ao + "\").");
-        } finally {
-            System.out.println("The result of the division is:");
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+
         return a / b;
     }
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3,0);
 
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3,2);
+            System.out.println("The result of the division is: " + result);
+        } catch (ArithmeticException ae) {
+            System.out.println("The divider can't equals 0. Chose different divider " + "(\"" + ae + "\").");
+        } finally {
+            System.out.println("Thank you.");
+        }
     }
 }
