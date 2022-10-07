0f62532e498d4a3e343b1c57a4a92e15c7f224a6
Marcin <marcin.macina@outlook.com>
2022-10-07 14:46:52 +0200

    14.2 default

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,11 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser implements User{
-//    final String username;
-//
-//    public ForumUser(String username) {
-//        this.username = "John Smith";
-//    }
 
     @Override
     public String getUserName() {
