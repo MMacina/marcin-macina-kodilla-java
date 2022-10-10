3b640faa8ae375fa9b5590ad06ab748eba1c66c8
Marcin <marcin.macina@outlook.com>
2022-10-10 20:23:19 +0200

    Singlemalt

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns.singleton;
+
+public class LoggerTestSuite {
+
+    //Bardzo przepraszam ale pojęcia nie mam co mam tu napisać.
+    // i co to są te logii...
+    //Odnosze wrażenie, że poziom abstrakcji tego modułu dalece wykracza poza moją percepcję.
+}
